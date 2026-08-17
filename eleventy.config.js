e1244b9ede594274c3656d0ca4214f914c85cab7
Piotr Jaskulski <piotr.jaskulski@gmail.com>
2026-08-17 15:58:20 +0200

    Make sitemap generation deterministic

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,6 +6,19 @@ export default function (eleventyConfig) {
         "src/favicon.ico": "favicon.ico",
     });
 
+    eleventyConfig.addCollection("sitemapPages", (collectionApi) =>
+        collectionApi
+            .getAll()
+            .filter(
+                (item) =>
+                    item.data.layout === "layouts/base.njk" &&
+                    item.data.robots !== "noindex, follow",
+            )
+            .sort((first, second) =>
+                first.url < second.url ? -1 : first.url > second.url ? 1 : 0,
+            ),
+    );
+
     return {
         dir: {
             input: "src",
